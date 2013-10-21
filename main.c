b1e2139ad54982fde673c6bc2879aaef3afd5da1
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-10-21 17:00:01 +0200

    testing in main function and centerDiff eval fix

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ typedef struct TCircle {
 
 int hasIntersection(TCIRCLE * circle1, TCIRCLE * circle2) {
     double centerDiff;
-    centerDiff = sqrt(pow(circle1->x - circle2->x, 2) + pow(&circle1->y - &circle2->y, 2)); 
+    centerDiff = sqrt(pow(circle1->x - circle2->x, 2) + pow(circle1->y - circle2->y, 2));
     if(centerDiff > (circle1->r + circle2->r)) return 0; // outside, no intersec.
     if(centerDiff == (circle1->r + circle2->r)) return -1; // outside, one intersec,
     if(centerDiff < (circle1->r + circle2->r) && centerDiff > abs(circle1->r - circle2->r)) return 2; // two intersec. -> evalVolume
     if(centerDiff < (circle1->r + circle2->r) && centerDiff == abs(circle1->r - circle2->r)) return 1; // inside, one intersec. -> evalVolume
     if(centerDiff < abs(circle1->r - circle2->r)) return 3; // inside, no intersec -> evalVolume
-    if(circle1->x == circle2->x && circle1.y == circle2.y && circle1.r == circle2.r) return 4; // identic -> evalVolume
+    if(circle1->x == circle2->x && circle1->y == circle2->y && circle1->r == circle2->r) return 4; // identic -> evalVolume
     return 100;
 }
 
@@ -49,10 +49,17 @@ double evalVolume(TCIRCLE circle1, TCIRCLE circle2) {
 }
 
 int main(int argc, char** argv) {
+    int inter;
+    TCIRCLE circle1, circle2;
+    printf("Zadejte parametry kruznice #1:\n");
+    if(!readCircle(circle1)) return 0;
+    printf("Zadejte parametry kruznice #2:\n");
+    if(!readCircle(circle2)) return 0;
     
-    TCIRCLE testCircle;
-    int status = readCircle(testCircle);
-    printf("%lf", testCircle.r);
+    inter = hasIntersection(&circle1, &circle2);
+    
+    
+    printf("%d", inter);
 
     return (EXIT_SUCCESS);
 }
